39b30a986146c5ca170f9fd1349376e0b3539816
Tony Robertson <tonyr729@gmail.com>
2018-12-13 11:49:21 -0700

    Adds authentication for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     validates_presence_of :name, :email, :password
     validates :email, uniqueness: true
+
+
+  def self.authenticate(email, password)
+    User.find_by(email: email, password: password)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,19 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_uniqueness_of(:email) }
   end
+
+  describe 'class methods' do
+    it 'should authenticate login' do
+      good_user = User.create(name: 'x', email: 'y', password: 'z')
+
+      user_1 = User.authenticate('y', 'z')
+      user_2 = User.authenticate('y', 'fail')
+      user_3 = User.authenticate('bob', 'fails')
+      
+      expect(user_1).to eq(good_user)
+      expect(user_2).to eq(nil)
+      expect(user_3).to eq(nil)
+    end
+  end
 end
  
